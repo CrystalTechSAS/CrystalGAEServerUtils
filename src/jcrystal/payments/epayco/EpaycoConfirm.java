ae6f8bb46f4019dfa4294bf634b42ac13d87c2b6
gasotelo <gasotelo@gmail.com>
2019-03-16 23:36:26 -0500

    Copyright

--- src/jcrystal/payments/epayco/EpaycoConfirm.java
+++ src/jcrystal/payments/epayco/EpaycoConfirm.java
@@ -1,3 +1,8 @@
+/* Copyright (C) Germán Augusto Sotelo Arévalo - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Germán Augusto Sotelo Arévalo <gasotelo@crystaltech.co>, December 2018
+ */
 package jcrystal.payments.epayco;
 
 import java.math.BigInteger;
