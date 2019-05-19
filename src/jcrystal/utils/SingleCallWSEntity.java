18f26adeecae0eeb5c1c697476994b00c16b50dc
gasotelo <gasotelo@gmail.com>
2019-05-18 22:34:08 -0500

    SingleCallWS

---
+++ src/jcrystal/utils/SingleCallWSEntity.java
@@ -0,0 +1,43 @@
+package jcrystal.utils;
+
+public final class SingleCallWSEntity {
+	
+	private static final String ENTITY_NAME = "SingleCallWSEntity";
+	
+	private final com.google.appengine.api.datastore.Entity rawEntity;
+	
+	private SingleCallWSEntity(com.google.appengine.api.datastore.Entity rawEntity){
+		this.rawEntity = rawEntity;
+	}
+	public  SingleCallWSEntity(String path){
+		rawEntity = new com.google.appengine.api.datastore.Entity(ENTITY_NAME, path);
+	}
+	public String path(){
+		return rawEntity.getKey().getName();
+	}
+	public SingleCallWSEntity put(){
+		jcrystal.context.CrystalContext.get().datastore.put(null, rawEntity);
+		return this;
+	}
+	private static com.google.appengine.api.datastore.Entity rawGet(String path){
+		if(null == path){return null;}
+		try{
+			return jcrystal.context.CrystalContext.get().datastore.get(null, com.google.appengine.api.datastore.KeyFactory.createKey(ENTITY_NAME, path));
+		}
+		catch(com.google.appengine.api.datastore.EntityNotFoundException | java.lang.IllegalArgumentException e){
+			return null;
+		}
+	}
+	public SingleCallWSEntity version(long version){
+		rawEntity.setUnindexedProperty("version", version);
+		return this;
+	}
+	public long version(){
+		return jcrystal.utils.EntityUtils.getLong(rawEntity, "version");
+	}
+	public static SingleCallWSEntity get(String path){
+		com.google.appengine.api.datastore.Entity ent = rawGet(path);
+		if(ent == null)return new SingleCallWSEntity(path).version(0);
+		return new SingleCallWSEntity(ent);
+	}
+}
