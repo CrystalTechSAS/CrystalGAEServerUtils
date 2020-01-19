f11cefd0b9742c2837b40386b852f240e3f33100
gasotelo <gasotelo@gmail.com>
2020-01-18 23:45:11 -0500

    Create EntitySet.java

---
+++ src/jcrystal/db/datastore/EntitySet.java
@@ -0,0 +1,70 @@
+package jcrystal.db.datastore;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.appengine.api.datastore.Entity;
+
+@SuppressWarnings({"rawtypes","unchecked"})
+public class EntitySet<T> implements Map<String, T>{
+	private Entity rawEntity;
+	public EntitySet(Entity rawEntity) {
+		this.rawEntity = rawEntity;
+	}
+	@Override
+	public void clear() {
+		rawEntity = new Entity("Map");
+	}
+	@Override
+	public boolean containsKey(Object arg0) {
+		return rawEntity.hasProperty((String)arg0);
+	}
+	@Override
+	public boolean containsValue(Object arg0) {
+		return false;
+	}
+	@Override
+	public Set<Entry<String, T>> entrySet() {
+		Map ret = rawEntity.getProperties();
+		return ((Map<String, T>)ret).entrySet();
+		
+	}
+	@Override
+	public T get(Object arg0) {
+		return (T)rawEntity.getProperty((String)arg0);
+	}
+	@Override
+	public boolean isEmpty() {
+		return rawEntity.getProperties().isEmpty();
+	}
+	@Override
+	public Set<String> keySet() {
+		return rawEntity.getProperties().keySet();
+	}
+	@Override
+	public T put(String arg0, T arg1) {
+		rawEntity.setUnindexedProperty(arg0, arg1);
+		return arg1;
+	}
+	@Override
+	public void putAll(Map<? extends String, ? extends T> arg0) {
+		arg0.forEach((k,v)->{
+			rawEntity.setUnindexedProperty(k, v);
+		});
+	}
+	@Override
+	public T remove(Object arg0) {
+		rawEntity.removeProperty((String)arg0);
+		return null;
+	}
+	@Override
+	public int size() {
+		return rawEntity.getProperties().size();
+	}
+	@Override
+	public Collection<T> values() {
+		Collection ret = rawEntity.getProperties().values();
+		return (Collection<T>)ret;
+	}
+}
